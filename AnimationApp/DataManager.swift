a4f67a07aea8e0532c2f09a6b1d7cdb7a5f9314a
DenysIlnytskyi <380984511271k@gmail.com>
2022-08-10 20:31:19 +0200

    Adding DataManager

---
+++ AnimationApp/DataManager.swift
@@ -0,0 +1,45 @@
+//
+//  DataManager.swift
+//  AnimationApp
+//
+//  Created by Денис Ільницький on 10/08/2022.
+//
+
+import Spring
+
+class DataManager {
+    
+    static let shared = DataManager()
+    
+    let animations: [Spring.AnimationPreset] = [
+        .slideLeft,
+        .slideRight,
+        .slideDown,
+        .slideUp,
+        .squeezeLeft,
+        .squeezeRight,
+        .squeezeDown,
+        .squeezeUp,
+        .fadeIn,
+        .fadeOut,
+        .fadeOutIn,
+        .fadeInLeft,
+        .fadeInRight,
+        .fadeInDown,
+        .fadeInUp,
+        .zoomIn,
+        .zoomOut,
+        .fall,
+        .shake,
+        .pop,
+        .flipX,
+        .flipY,
+        .morph,
+        .squeeze,
+        .flash,
+        .wobble,
+        .swing
+    ]
+    
+    private init() {}
+}
